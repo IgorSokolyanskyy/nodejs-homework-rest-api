165254b7cce197964116a4c2ad9d515791a252f8
IgorSokolyanskyy <1988isokol@gmail.com>
2023-04-06 14:39:12 +0300

    fix
    исправил замечания

--- src/routes/api/contacts.js
+++ src/routes/api/contacts.js
@@ -12,7 +12,7 @@ router.get("/:contactId", ctrl.getById);
 
 router.post("/", validateBody(schema.addSchema), ctrl.add);
 
-router.put("/:contactId", validateBody(schema.addSchema), ctrl.updateById);
+router.put("/:contactId", validateBody(schema.updateSchema), ctrl.updateById);
 
 router.delete("/:contactId", ctrl.removeById);
 
