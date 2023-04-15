ae8f4b45d91a0e55816e127e15dad0bf3a65e77b
IgorSokolyanskyy <1988isokol@gmail.com>
2023-04-15 23:43:30 +0300

    create patch

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -4,7 +4,10 @@ const ctrlWrapper = require("./ctrlWrapper");
 
 const httpError = require("./httpError");
 
+const handleMongooseErorr = require("./handleMongooseErorr");
+
 module.exports = {
   ctrlWrapper,
   httpError,
+  handleMongooseErorr,
 };
