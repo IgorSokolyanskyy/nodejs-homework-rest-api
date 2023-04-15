33e16f230fefc836267864900bdbf7ddb92d25fb
IgorSokolyanskyy <1988isokol@gmail.com>
2023-04-15 18:39:30 +0300

    render

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const contactsRouter = require("./src/routes/api/contacts");
+const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
